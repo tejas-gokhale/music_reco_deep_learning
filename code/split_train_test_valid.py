f0ce96d540d786e131ac23a672181e386560ff4b
Tejas <tejas93.gokhale@gmail.com>
2017-05-07 20:54:37 -0400

    faltu

--- code/split_train_test_valid.py
+++ code/split_train_test_valid.py
@@ -12,19 +12,3 @@ with open("../data/try.csv") as fr:
                     f = f2
                 else:
                     f = f3
-=======
-import random
-
-with open("../data/try.csv") as fr:
-    with open("../data/data_train.csv", "w") as f1, open("../data/data_test.csv", "w") as f2, open("../data/data_valid.csv", "w") as f3:
-        for line in fr.readlines():
-            rd = random.random()
-            if rd <= 0.7:
-                f = f1
-            else:
-                if rd <=0.9:
-                    f = f2
-                else:
-                    f = f3
->>>>>>> cd32d1e442a63751bf269452cb62c48a8be8bba1
-            f.write(line)
