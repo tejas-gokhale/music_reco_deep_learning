9340daaa9f6fbafe6bc4df60d5e26ea627f3e416
Deepika Bablani <dipikabablani@gmail.com>
2017-05-09 21:51:51 -0400

    Add files via upload

--- code/split_train_test_valid.py
+++ code/split_train_test_valid.py
@@ -2,14 +2,17 @@ import random
 
 def split_data(file):
     with open(file) as fr:
-        with open("../data/all/data_train.csv", "w") as f1, open("../data/all/data_test.csv", "w") as f2, open("../data/all/data_valid.csv", "w") as f3:
+        with open("../data/new/data_train.csv", "w") as f1, open("../data/new/data_test.csv", "w") as f2, open("../data/new/data_valid.csv", "w") as f3:
             for line in fr.readlines():
                 rd = random.random()
-                if rd <= 0.7:
+                if rd <= 0.4:
                     f = f1
                 else:
-                    if rd <=0.9:
+                    if rd <=0.8:
                         f = f2
                     else:
                         f = f3
                 f.write(line)
+
+
+split_data('../data/inputs.csv')
