3265cea53efb12f6c0092a901830998d02a5f722
raghavgupta12 <raghavg@andrew.cmu.edu>
2017-05-09 21:48:43 -0400

    Delete zzz.py

--- code/zzz.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# csv = np.genfromtxt ('../data/all/inputs.csv', delimiter=",")
-# print(csv.shape)
-
-from split_train_test_valid import split_data
-
-split_data('../data/all/inputs.csv')
